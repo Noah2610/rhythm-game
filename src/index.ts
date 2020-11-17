3d067277860d758253d44cb56c139da4dd19ef31
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-17 08:50:32 +0100

    Setup game beat-target styling
    Split CSS into `base.css` and `game.css`.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,21 @@
 function main() {
-    console.log("Hello World!");
+    setupStartButton();
+}
+
+function setupStartButton() {
+    const startBtnEl = document.querySelector("#btn-start");
+    startBtnEl?.addEventListener("click", () => {
+        const parentEl = startBtnEl.parentElement;
+        if (parentEl && !parentEl.classList.contains("hidden")) {
+            parentEl.classList.add("hidden");
+            startGame();
+        }
+    });
+}
+
+function startGame() {
+    const gameEl = document.querySelector("#game");
+    gameEl?.classList.remove("hidden");
 }
 
 window.onload = main;
