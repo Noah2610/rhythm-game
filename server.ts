5cc5cfa959b047b2abe7c7cc77506c41d238ec14
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-17 07:41:42 +0100

    Setup working file server

--- server.ts
+++ server.ts
@@ -1,1 +1,19 @@
-console.log("Hello Server!");
+import express from "express";
+import path from "path";
+
+const PORT = parseInt(process.env.PORT || "") || 8080;
+const HOSTNAME = process.env.HOST || "0.0.0.0";
+
+const app = express();
+
+app.get("/", (_req, res) => {
+    res.sendFile(path.resolve("./public/index.html"));
+});
+
+app.get("/:file*", (req, res) => {
+    res.sendFile(path.resolve(`./public/${req.params.file}`));
+});
+
+app.listen(PORT, HOSTNAME, () =>
+    console.log(`Server running on http://${HOSTNAME}:${PORT}`),
+);
