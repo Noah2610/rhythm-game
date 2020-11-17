3dd247ae5f40d1e79022ff2ca6883f39b223e656
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-17 13:05:47 +0100

    Refactor map loading and styling a bit

--- src/game.ts
+++ src/game.ts
@@ -2,7 +2,6 @@ import { MapConfig } from "./map-config";
 
 export async function startGame() {
     await loadMap("dev.json");
-
     dom.game.classList.remove("hidden");
 }
 
@@ -13,27 +12,22 @@ async function loadMap(mapName: string) {
         `/maps/${mapName}`,
     ).then((response) => response.json());
 
-    const beatTargetLineEl = document.createElement("div");
-    beatTargetLineEl.classList.add("beat-target-line");
-
     for (const key of mapConfig.layout.keys) {
         const beatTargetEl = document.createElement("div");
         beatTargetEl.classList.add("beat-target");
         beatTargetEl.innerText = key;
-        beatTargetLineEl.appendChild(beatTargetEl);
+        dom.beatTargetLine.appendChild(beatTargetEl);
     }
-
-    dom.game.appendChild(beatTargetLineEl);
 }
 
 function resetGame() {
-    const gameEl = document.querySelector("#game");
-    gameEl && (gameEl.innerHTML = "");
+    dom.beatTargetLine.innerHTML = "";
+    dom.beatSpawnLine.innerHTML = "";
 }
 
 const dom: DomHelper = {
-    query(query: string) {
-        const el = document.querySelector(query);
+    queryExpect(query, rootEl = undefined) {
+        const el = (rootEl || document).querySelector(query);
         if (!el) {
             throw new Error(`Element with query ${query} not found.`);
         }
@@ -42,20 +36,71 @@ const dom: DomHelper = {
 
     get game() {
         if (!this.cache.game) {
-            this.cache.game = this.query("#game");
+            this.cache.game = this.queryExpect("#game");
         }
         return this.cache.game;
     },
 
+    get beatTargetLine() {
+        if (!this.cache.beatTargetLine) {
+            this.cache.beatTargetLine = this.queryExpect(
+                ".beat-target-line",
+                this.game,
+            );
+        }
+        return this.cache.beatTargetLine;
+    },
+
+    get beatSpawnLine() {
+        if (!this.cache.beatSpawnLine) {
+            this.cache.beatSpawnLine = this.queryExpect(
+                ".beat-spawn-line",
+                this.game,
+            );
+        }
+        return this.cache.beatSpawnLine;
+    },
+
     cache: {
         game: null,
+        beatTargetLine: null,
+        beatSpawnLine: null,
     },
 };
 
 interface DomHelper {
-    query: (query: string) => Element;
+    /**
+     * Run a `document.querySelector` with the given `query`,
+     * but expect the element to exist.
+     * Throws an error if it doesn't exist.
+     *
+     * Optionally pass the `rootEl`, on which `.querySelector` is called.
+     * Otherwise `document` is used.
+     */
+    queryExpect: (query: string, rootEl?: Element) => Element;
+
+    /**
+     * The game wrapper element.
+     */
     game: Element;
+
+    /**
+     * The game's wrapper element for beat-targets.
+     */
+    beatTargetLine: Element;
+
+    /**
+     * The game's wrapper element for beat-spawns.
+     */
+    beatSpawnLine: Element;
+
+    /**
+     * Internally used cache for queried elements.
+     * Shouldn't be used outside of the `DomHelper`'s own functions.
+     */
     cache: {
         game: Element | null;
+        beatTargetLine: Element | null;
+        beatSpawnLine: Element | null;
     };
 }
