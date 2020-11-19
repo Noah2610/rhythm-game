fa5248e46e1ad98272f336463bd9555cfe97fc1f
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-19 02:35:31 +0100

    Setup game udpate loop, setup beat spawn styling, minor CSS refactor

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,7 +1,22 @@
 import dom from "./dom-helper";
 import { loadMap } from "./map-loader";
 
+const UPDATE_INTERVAL_MS = 1000.0 / 60.0;
+let updateGameIntervalId: NodeJS.Timeout | null = null;
+
 export async function startGame() {
     await loadMap("dev.json");
     dom.game.classList.remove("hidden");
+
+    updateGameIntervalId = setInterval(updateGame, UPDATE_INTERVAL_MS);
+}
+
+export function stopGame() {
+    if (updateGameIntervalId) {
+        clearInterval(updateGameIntervalId);
+    }
+}
+
+function updateGame() {
+    console.log("UPDATE");
 }
