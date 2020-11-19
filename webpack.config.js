da5edc6ef12847d953a2b26f88f033c2da76c07c
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-19 02:13:36 +0100

    Rework build pipeline
    Build server with `webpack` too now.
    Remove `typescript-bundle` dependency.
    `webpack.config.js` exports different config depending
    on `$TARGET` environment variable ("app" or "server").
    Rework npm scripts.

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,61 @@
 const path = require("path");
 
-module.exports = {
-    entry: "./src/index.ts",
-    devtool: "inline-source-map",
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                use: "ts-loader",
-                exclude: /node_modules/,
-            },
-        ],
+const CONFIGS = {
+    app: {
+        entry: "./src/index.ts",
+        devtool: "inline-source-map",
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: "ts-loader",
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".ts"],
+        },
+        output: {
+            filename: "bundle.js",
+            path: path.resolve(__dirname, "public"),
+        },
     },
-    resolve: {
-        extensions: [".ts"],
-    },
-    output: {
-        filename: "bundle.js",
-        path: path.resolve(__dirname, "public"),
+
+    server: {
+        entry: "./server.ts",
+        target: "node",
+        module: {
+            rules: [
+                {
+                    test: /\.ts$/,
+                    use: [
+                        {
+                            loader: "ts-loader",
+                            options: {
+                                configFile: "tsconfig.server.json",
+                            },
+                        },
+                    ],
+                    exclude: /node_modules/,
+                },
+            ],
+        },
+        resolve: {
+            extensions: [".js", ".ts"],
+        },
+        output: {
+            filename: "server.js",
+            path: path.resolve(__dirname),
+        },
     },
 };
+
+const target = process.env.TARGET || "app"; // "app" || "server"
+
+if (!["app", "server"].includes(target)) {
+    console.error('Environment variable `TARGET` must be "app" or "server"');
+    process.exit(1);
+}
+
+module.exports = CONFIGS[target];
