b734c362cc1b238ccf57485cb8f305ac78655ca5
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-11-20 11:06:35 +0100

    Refactor HitState stuff to new module game/hit-state.ts

--- src/game/index.ts
+++ src/game/index.ts
@@ -1,5 +1,6 @@
 import { BeatSettings, BeatSpawnConfig, MapConfig } from "../map-config";
 import dom from "./dom-helper";
+import { getHitStateFromDist, HitState } from "./hit-state";
 import { loadMap } from "./map-loader";
 
 const UPDATE_INTERVAL_MS = 1000.0 / 60.0;
@@ -166,51 +167,3 @@ function checkBeatHit(key: string) {
         console.log(hitState);
     }
 }
-
-function getHitStateFromDist(dist: number): HitState {
-    return (
-        [HitState.Perfect, HitState.Early, HitState.Late, HitState.Missed].find(
-            (hitState) =>
-                dist >= BEAT_HIT_STATE_RANGES[hitState].low &&
-                dist < BEAT_HIT_STATE_RANGES[hitState].high,
-        ) || HitState.Missed
-    );
-}
-
-enum HitState {
-    Perfect = "Perfect",
-    Early = "Early",
-    Late = "Late",
-    Missed = "Missed",
-}
-
-interface Range {
-    low: number;
-    high: number;
-}
-
-interface BeatHitStateRanges {
-    [HitState.Perfect]: Range;
-    [HitState.Early]: Range;
-    [HitState.Late]: Range;
-    [HitState.Missed]: Range;
-}
-
-const BEAT_HIT_STATE_RANGES: BeatHitStateRanges = {
-    [HitState.Perfect]: {
-        low: 0,
-        high: 32,
-    },
-    [HitState.Early]: {
-        low: 32,
-        high: 128,
-    },
-    [HitState.Late]: {
-        low: -32,
-        high: 0,
-    },
-    [HitState.Missed]: {
-        low: 128,
-        high: Infinity,
-    },
-};
