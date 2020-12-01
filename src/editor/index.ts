cd8274a7344dfaf04fd4bc12cacbc293fba5e9f7
Noah Rosenzweig <rosenzweig.noah@gmail.com>
2020-12-01 15:33:32 +0100

    Editor can load user given audio files

--- src/editor/index.ts
+++ src/editor/index.ts
@@ -1,8 +1,39 @@
 export function startEditor() {
-    const loadSongEl = document.querySelector("#editor #btn-load-song");
+    const loadSongEl = document.querySelector(
+        "#editor #btn-load-song",
+    ) as HTMLInputElement;
     if (loadSongEl) {
-        loadSongEl.addEventListener("change", (event) => {
-            console.log(event)
+        loadSongEl.addEventListener("change", () => {
+            const file = (loadSongEl.files || [])[0];
+            if (file) {
+                loadAudio(file);
+            }
         });
     }
+}
+
+async function loadAudio(file: File): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const fileReader = new FileReader();
+
+        fileReader.onerror = () => {
+            reject("Failed to load audio.");
+        };
+
+        fileReader.onload = () => {
+            const audioSrc = fileReader.result as string;
+            if (!audioSrc) {
+                reject(`Invalid audio dataURI src: ${audioSrc}`);
+                return;
+            }
+            const audioEl = new Audio();
+            audioEl.id = "song";
+            audioEl.classList.add("hidden");
+            audioEl.src = audioSrc;
+            document.querySelector("#editor")?.appendChild(audioEl);
+            resolve();
+        };
+
+        fileReader.readAsDataURL(file);
+    });
 }
